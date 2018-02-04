5d3ba8fa0354e25de312ca37b2aebb73d8efbc7f
Matthew Kindy II <matt.kindy.ii@gmail.com>
2018-02-04 11:28:57 -0600

    Modified to match the *different* proof setup in OP. Most pieces for NIZK connected

--- src/main/scala/bifrost/consensus/F_VRF/DiscreteLogProof.scala
+++ src/main/scala/bifrost/consensus/F_VRF/DiscreteLogProof.scala
@@ -9,18 +9,17 @@ import scorex.crypto.hash.Blake2b256
 /**
   * NIZK proof that log_g(y) = log_a(b), covered by random t
   *
-  * @param w  derived as the hash: H(g, y, a, b, g&#94;t, a&#94;t)
+  * @param c  derived as the hash: H(g, y, a, b, g&#94;t, a&#94;t)
   * @param s  dervied as t + w * k % g.mod
   */
-case class DiscreteLogProof(w: BigInt, s: BigInt) extends Proof[DiscreteLogProposition] {
+case class DiscreteLogProof(c: BigInt, s: BigInt) extends Proof[DiscreteLogProposition] {
 
   override type M = DiscreteLogProof
 
   def serializer: Serializer[DiscreteLogProposition] = ???
 
-  // TODO implement verification of w,s by proposition
   override def isValid(proposition: Proposition, sample: Array[Byte]): Boolean = proposition match {
-    case p: DiscreteLogProposition => ???
+    case p: DiscreteLogProposition => p.verify(this, sample)
     case _ => false
   }
 }
@@ -28,24 +27,26 @@ case class DiscreteLogProof(w: BigInt, s: BigInt) extends Proof[DiscreteLogPropo
 object DiscreteLogProof {
 
   /**
+    * @param m  argument used as parameter for PRF
     * @param g  generator of a group of prime order m
-    * @param y  public key generated originally from g&#94;k, k in Z_m
-    * @param a  H(x)&#94;r, where H: Z => {0,1}&#94;lambda
+    * @param v  public key generated originally from g&#94;k, k in Z_m
+    * @param a  H(m)&#94;r, where H: Z => {0,1}&#94;lambda
     * @param b  a&#94;k
     * @param k
     */
-  def apply(g: BigInt, y: BigInt, a: BigInt, b: BigInt, k: PrivateKey25519): DiscreteLogProof = {
-    val (w: BigInt, s: BigInt) = {
-      val t: BigInt = ??? // randomly selected from Z_m
-      val w: BigInt = BigInt(hash(g, y, a, b, g^t, a^t))
-      val s: BigInt = t + w * BigInt(k.privKeyBytes) % g // needs to  be mod m, not g
-      (w, s)
+  def apply(m: BigInt, g: BigInt, v: BigInt, a: BigInt, b: BigInt, k: PrivateKey25519): DiscreteLogProof = {
+    val (c: BigInt, s: BigInt) = {
+      val r: BigInt = ??? // randomly selected from Z_m
+      val c: BigInt = BigInt(hash(m, v, g^r, a))
+      val s: BigInt = r + c * BigInt(k.privKeyBytes) % g // needs to  be mod q, not g
+      (c, s)
     }
 
-    DiscreteLogProof(w, s)
+    DiscreteLogProof(c, s)
   }
 
-  // TODO this is just a placeholder hash!
+  // TODO these are just placeholder hashes!
   def hash(inputs: BigInt*): Blake2b256.Digest = scorex.crypto.hash.Blake2b256(inputs.foldLeft(new Array[Byte](0))((a: Array[Byte], b) => a ++ b.toByteArray))
+  def groupHash(inputs: BigInt*): BigInt = BigInt(scorex.crypto.hash.Blake2b256(inputs.foldLeft(new Array[Byte](0))((a: Array[Byte], b) => a ++ b.toByteArray)))
 
 }
--- src/main/scala/bifrost/consensus/F_VRF/DiscreteLogProposition.scala
+++ src/main/scala/bifrost/consensus/F_VRF/DiscreteLogProposition.scala
@@ -1,19 +1,26 @@
 package bifrost.consensus.F_VRF
 
 import scorex.core.serialization.Serializer
+import scorex.core.transaction.box.proposition.Proposition
+import scorex.core.transaction.proof.Proof
 
 /**
-  * Proposition that log_g(y) = log_a(b)
+  * Proposition that log_g(v) = log_(H(m))(u)
   *
-  * @param g  generator of a group of prime order m
-  * @param y  public key generated originally from g&#94;k, k in Z_m
-  * @param a  H(x)&#94;r, where H: Z => {0,1}&#94;lambda
-  * @param b  a&#94;k
+  * @param m  argument used as parameter for PRF
+  * @param g  generator of a group of prime order q
+  * @param v  public key generated originally from g&#94;k, k in Z_m
+  * @param u  H(m)&#94;k
   */
-case class DiscreteLogProposition(g: BigInt, y: BigInt, a: BigInt, b: BigInt) extends EqualityProposition[(BigInt, BigInt), BigInt, DiscreteLog] {
+case class DiscreteLogProposition(m: BigInt, g: BigInt, v: BigInt, u: BigInt) extends EqualityProposition[(BigInt, BigInt), BigInt, DiscreteLog] {
 
   override type M = DiscreteLogProposition
   def serializer: Serializer[DiscreteLogProposition] = ???
 
-  def verify(input1: (BigInt, BigInt), input2: (BigInt, BigInt), proof: Array[Byte]): Boolean = ???
+  def verify(proof: Proof[Proposition], sample: Array[Byte]): Boolean = proof match {
+    case d: DiscreteLogProof =>
+      d.c.equals(BigInt(DiscreteLogProof.hash(m, v, g^d.s / v^d.c, DiscreteLogProof.groupHash(m)^d.s/u^d.c))) &&
+        sample.sameElements(DiscreteLogProof.hash(m, u))
+    case _ => false
+  }
 }
